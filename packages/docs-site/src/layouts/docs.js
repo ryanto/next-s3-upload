106b20279007a23ba871dc168a4ef4da7c76db3e
Ryan Toronto <ryanto@gmail.com>
2022-12-03 14:25:34 -0500

    spacing

--- packages/docs-site/src/layouts/docs.js
+++ packages/docs-site/src/layouts/docs.js
@@ -58,7 +58,7 @@ export default function({ children, tableOfContents }) {
             <li className="pt-1 border-l border-gray-200">
               <DocLink href="/multi-file-uploads">Multiple files</DocLink>
             </li>
-            <li className="pt-1 pb-1 border-l border-gray-200">
+            <li className="pt-1 border-l border-gray-200">
               <DocLink href="/custom-file-input">Custom file input</DocLink>
             </li>
             <li className="pt-1 pb-1 border-l border-gray-200">
