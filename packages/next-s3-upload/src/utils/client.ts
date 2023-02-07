a4210e8c4ac66a288c49f4e773d26e51b087311b
Manuel Sechi <msechi@gmail.com>
2023-02-07 02:11:02 +0000

    make aws credentials optional

--- packages/next-s3-upload/src/utils/client.ts
+++ packages/next-s3-upload/src/utils/client.ts
@@ -1,10 +1,19 @@
 import { S3Client } from '@aws-sdk/client-s3';
 import { getConfig, S3Config } from './config';
 
+export type credentials = {
+  accessKeyId: string;
+  secretAccessKey: string;
+};
+export type configParams = {
+  credentials?: credentials
+  region: string
+};
+
 export function getClient(s3Config?: S3Config) {
   let config = getConfig(s3Config);
 
-  let client = new S3Client({
+  const configParams: configParams = {
     credentials: {
       accessKeyId: config.accessKeyId,
       secretAccessKey: config.secretAccessKey,
@@ -12,7 +21,13 @@ export function getClient(s3Config?: S3Config) {
     region: config.region,
     ...(config.forcePathStyle ? { forcePathStyle: config.forcePathStyle } : {}),
     ...(config.endpoint ? { endpoint: config.endpoint } : {}),
-  });
+  }
+
+  if(!config.accessKeyId || !config.secretAccessKey) {
+    delete configParams.credentials;
+  }
+
+  let client = new S3Client(configParams);
 
   return client;
 }
