d827bbfacd30d43f896cddd0007d788c6359bb2a
Junjun Deng <cnjamesdeng@gmail.com>
2024-08-11 21:02:21 +0800

    Update code to upload to backblaze

--- test-apps/app-dir/src/app/api/s3-upload/route.ts
+++ test-apps/app-dir/src/app/api/s3-upload/route.ts
@@ -1,1 +1,11 @@
-export { POST } from "next-s3-upload/route";
+import { POST as handler } from "next-s3-upload/route";
+
+// import { APIRoute } from "next-s3-upload";
+
+export const POST = handler.configure({
+  accessKeyId: process.env.S3_UPLOAD_KEY,
+  secretAccessKey: process.env.S3_UPLOAD_SECRET,
+  bucket: process.env.S3_UPLOAD_BUCKET,
+  region: process.env.S3_UPLOAD_REGION ?? "us-east-1",
+  endpoint: "https://s3.us-west-004.backblazeb2.com"
+})
